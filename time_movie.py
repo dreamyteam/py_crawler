9e8374d69410d54d451934ac2f7440cab1ffd335
lycray <814021113@qq.com>
2016-06-03 21:01:32 +0800

    时光网更新信息

--- time_movie.py
+++ time_movie.py
@@ -38,11 +38,13 @@ def search_name(name):
 # 	# print u'查询的电影:%s' % j
 
 # 	break
-
+single_test = [
+				'http://movie.mtime.com/11925/',
+			]
 movie_url_list = [
+					'http://movie.mtime.com/196613/',
 					'http://movie.mtime.com/164011/',
 					'http://movie.mtime.com/11925/',
-					'http://movie.mtime.com/196613/',
 					'http://movie.mtime.com/216573/',
 					'http://movie.mtime.com/209122/',
 					'http://movie.mtime.com/134727/',
@@ -73,44 +75,70 @@ def staff_info(sel4,staff_list_info, selector_list_info):
 			index_info = selector_list_info.index(staff_list[i]) + 1
 			for j in sel4.xpath('//*[@class="credits_r"]/div[{0}]/p'.format(index_info,)):
 				people_info = j.xpath('./a/text()').extract()[0].strip()
-				# print u'{0}:{1}'.format(staff_list[i], people_info)
-				new_info_list.append(people_info)
-			update_dict[i] = new_info_list
+				people_url = j.xpath('./a/@href').extract()
+				if people_url:
+					people_url = people_url[0].strip()
+				else:
+					people_url = ''
+				insert_dict = {
+								'name': people_info,
+								'url': people_url,
+							}
 
+				# print u'{0}:{1}'.format(staff_list[i], people_info)
+				new_info_list.append(insert_dict)
+			update_dict[i] = new_info_list
 	return update_dict
 
 #制作公司和发行公司
 def company_type(sel2):
 	company_list = list()
-	sale_company_list = list()
+	distributors_list = list()
 	add_dict = dict()
 	if len(sel2.xpath('//*[@id="companyRegion"]/dd/div[1]/div')) <= 2:
 		if sel2.xpath('//*[@id="companyRegion"]/dd/div[1]/div[1]/ul/li'):
 			for i in sel2.xpath('//*[@id="companyRegion"]/dd/div[1]/div[1]/ul/li'):
 					company_name = i.xpath('./a/text()').extract()[0].strip()
+					if i.xpath('./a/@href').extract():
+						company_url = i.xpath('./a/@href').extract()[0].strip()
+					else:
+						company_url = ''
+					product_dict = {
+									'name': company_name,
+									'url': company_url,
+								}
 					# print u'公司:%s' % company_name
-					company_list.append(company_name)
+					company_list.append(product_dict)
+
 			if sel2.xpath('//*[@id="companyRegion"]/dd/div[1]/div[1]/h4/text()').extract()[0].strip() == u'制作公司':
-				add_dict['made_company'] = company_list
+				add_dict['production_company'] = company_list
 				if sel2.xpath('//*[@id="companyRegion"]/dd/div[1]/div[2]/h4/text()'):
 					for i in sel2.xpath('//*[@id="companyRegion"]/dd/div[1]/div[2]/ul/li'):
 						sale_company_name = i.xpath('./a/text()').extract()[0].strip()
+						if i.xpath('./a/@href').extract():
+							sale_company_url = i.xpath('./a/@href').extract()[0].strip()
+						else:
+							sale_company_url = ''
 						# print u'发行公司:%s' % sale_company_name
-						sale_company_list.append(sale_company_name)
-					add_dict['sale_company'] = sale_company_list
-
+						sale_dict = {
+										'name': sale_company_name,
+										'url': sale_company_url,
+									}
+						distributors_list.append(sale_dict)
+					add_dict['distributors'] = distributors_list
 					return add_dict
 			else:
-				add_dict['sale_company'] = company_list
-				
+				add_dict['distributors'] = company_list
 				return add_dict
-
 
 #影片基本信息
 import bson
 def movie_info(url):
 
 	time_movie_info = dict()
+	time_movie_info['movie_url'] = url
+	time_movie_info['source'] = 'time'
+	time_movie_info['movie_id'] = url.split('/')[-2]
 	response = urllib2.urlopen(url) #影片起始页面
 	response2 = urllib2.urlopen(url + 'details.html') #影片更多资料
 	response3 = urllib2.urlopen(url + 'plots.html') #影片简介
@@ -123,7 +151,7 @@ def movie_info(url):
 	data3 = response3.read()
 	data4 = response4.read()
 	data5 = response5.read()
-	print u'状态码:%s' % response5.code
+	# print u'状态码:%s' % response5.code
 	sel = Selector(text=data)
 	sel2 = Selector(text=data2)
 	sel3 = Selector(text=data3)
@@ -142,11 +170,12 @@ def movie_info(url):
 	print u'电影名称:%s' % time_movie_info['movie_name']
 	#2 英文名称
 	is_en_name = sel2.xpath('//*[@class="db_enname"]/a/text()').extract()
-	time_movie_info['en_name'] = ''
+	time_movie_info['foreign_name'] = ''
 	if is_en_name:
 		en_name = sel2.xpath('//*[@class="db_enname"]/a/text()').extract()[0].strip()
-		time_movie_info['en_name'] = en_name
-	print u'英文名称:%s' % time_movie_info['en_name']
+		time_movie_info['foreign_name'] = en_name
+
+	# print u'外文名称:%s' % time_movie_info['foreign_name']
 	#3/5电影片长和别名
 	is_exist = sel2.xpath('//*[@class="pb12"]')
 	time_movie_info['other_name'] = list()
@@ -154,8 +183,8 @@ def movie_info(url):
 	if is_exist:
 		if len(is_exist) == 1:
 			movie_time_long = is_exist[0].xpath('./p/text()').extract()[0].strip()
-			time_movie_info['movie_time_long'] = movie_time_long
-			print u'电影片长:%s' %  time_movie_info['movie_time_long']
+			time_movie_info['runtime'] = movie_time_long
+			# print u'电影片长:%s' %  time_movie_info['runtime']
 
 		elif len(is_exist) > 1:
 			for i in is_exist[:-1]:
@@ -163,23 +192,23 @@ def movie_info(url):
 				for j in other_name_list:
 					other_name = j.xpath('./text()').extract()[0].strip()
 					time_movie_info['other_name'].append(other_name)
-					print u'别名:%s' % other_name
+					# print u'别名:%s' % other_name
 
 			movie_time_long = is_exist[-1].xpath('./p/text()').extract()[0].strip()
-			time_movie_info['movie_time_long'] = movie_time_long
+			time_movie_info['runtime'] = movie_time_long
 			# print u'电影片长:%s' %  time_movie_info['movie_time_long']
 			# print u'别名列表:%s' % time_movie_info['other_name']
 	#6影片类型
 	is_type = sel.xpath('//*[@property="v:genre"]')
-	time_movie_info['movie_type'] = list()
+	time_movie_info['film_type'] = list()
 	if is_type:
 		for i in is_type:
 			movie_type = i.xpath('./text()').extract()[0].strip()
-			print u'影片类型:%s' % movie_type
-			time_movie_info['movie_type'].append(movie_type)
-	print u'影片类型列表:%s' % time_movie_info['movie_type']
+			# print u'影片类型:%s' % movie_type
+			time_movie_info['film_type'].append(movie_type)
+	# print u'影片类型列表:%s' % time_movie_info['film_type']
 	#7/8 简介和IMDB编号
-	time_movie_info['imdb_id'] = ''
+	time_movie_info['IMDB_ID'] = ''
 	time_movie_info['introduce'] = list()
 	is_introduce = sel3.xpath('//*[@class="plots_box"]')
 	if is_introduce:
@@ -187,11 +216,10 @@ def movie_info(url):
 			introduce1 = i.xpath('.//*[@class="first_letter"]/text()').extract()[0].strip()
 			introduce_list = i.xpath('./div[2]//p/text()').extract()
 			introduce2 = ''.join(introduce_list)
-			print u'简介:{0}{1}'.format(introduce1, introduce2)
+			# print u'简介:{0}{1}'.format(introduce1, introduce2)
 			time_movie_info['introduce'].append(introduce1+introduce2)
 		# print u'简介列表:%s' % time_movie_info['introduce']
-	#9首映时间
-	time_movie_info['first_release'] = ''
+
 	#10 制作国家/地区	
 	is_info_l = sel.xpath('//*[@pan="M14_Movie_Overview_BaseInfo"]')
 	time_movie_info['area'] = ''
@@ -205,7 +233,7 @@ def movie_info(url):
 	if is_area != None:
 		area = sel.xpath('//*[@pan="M14_Movie_Overview_BaseInfo"][{0}]/a/text()'.format(is_area,)).extract()[0].strip()
 		time_movie_info['area'] = area
-		print u'制作国家和地区:%s' % time_movie_info['area']
+		# print u'制作国家和地区:%s' % time_movie_info['area']
 
 	#11/12上映时间的地区
 	is_release = sel2.xpath('//*[@class="db_showdate"]')
@@ -215,9 +243,10 @@ def movie_info(url):
 		for i in release_list[1:]:
 			countryname = i.xpath('./div[1]/p/text()').extract()[0].strip()
 			datecont = i.xpath('./div[2]/text()').extract()[0].strip()
-			print u'{0}*****{1}'.format(countryname, datecont)
+			# print u'{0}*****{1}'.format(countryname, datecont)
 			save_list = [countryname, datecont]
 			time_movie_info['release_time_area'].append(save_list)
+
 	#13语言
 	is_language = sel2.xpath('//*[@class="db_movieother_2"]/dl')
 	time_movie_info['language'] = list()
@@ -236,7 +265,7 @@ def movie_info(url):
 		# print language_list
 		for i in language_list:
 			language = i.xpath('./text()').extract()[0].strip().split('/')[0].strip()
-			print u'对白语言:%s' % language
+			# print u'对白语言:%s' % language
 			time_movie_info['language'].append(language)
 
 	#14导演-24武术指导
@@ -246,7 +275,7 @@ def movie_info(url):
 		for i in is_all:
 			staff_people = i.xpath('./h4/text()').extract()[0].strip()
 			selector_list_info.append(staff_people)
-			print staff_people
+			# print staff_people
 
 	update_info = staff_info(sel4, staff_list, selector_list_info)
 	# for x in update_info:
@@ -256,11 +285,10 @@ def movie_info(url):
 
 	#27制作公司/28发行公司
 	if sel2.xpath('//*[@id="companyRegion"]'):
-		company_type(sel2)
+		time_movie_info.update(company_type(sel2))
 	else:
-		time_movie_info['made_company'] = list()
-		time_movie_info['sale_company'] = list()
-
+		time_movie_info['production_company'] = list()
+		time_movie_info['distributors'] = list()
 	#28主演/29饰演角色
 	is_actor = sel4.xpath('//*[@class="db_actor"]/dl')
 	time_movie_info['actor_charactor'] = list()
@@ -269,37 +297,65 @@ def movie_info(url):
 		for i in is_actor:
 			for j in i.xpath('./dd'):
 				actor = j.xpath('.//*[@class="actor_tit"]//h3/a/text()').extract()[0].strip()
+				if j.xpath('.//*[@class="actor_tit"]//h3/a/@href').extract():
+					actor_url = j.xpath('.//*[@class="actor_tit"]//h3/a/@href').extract()[0].strip()
+				else:
+					actor_url = ''
+				actor_dict = {
+								'name': actor,
+								'url': actor_url,
+							}
 
 				charactor = j.xpath('.//*[@class="character_inner"]//h3/a/text() | .//*[@class="character_inner"]//h3/text()').extract()
-				if not charactor:
-					charactor = ''
-				else:
+				if charactor:
 					charactor = j.xpath('.//*[@class="character_inner"]//h3/a/text() | .//*[@class="character_inner"]//h3/text()').extract()[0].strip()
-					
-				print u'演员:{0}\t\t角色:{1}'.format(actor, charactor)
-				time_movie_info['actor_charactor'].append([actor, charactor])
+				else:
+					charactor = ''
+				# print u'演员:{0}\t\t角色:{1}'.format(actor, charactor)
+				time_movie_info['actor_charactor'].append([actor_dict, charactor])
+
 	#29获奖记录 30提名
 	if response5.url == 'http://www.mtime.com/404.html':
+		time_movie_info['award_nominate'] = list()
 
-		time_movie_info['awards_record'] = [u'未开始',]
-		time_movie_info['awards_get'] = [u'未开始',]
 	else:
-		print '执行这个？'
+
 		awards_record(sel5)
 
 def awards_record(sel5):
-	print u'长度:%s' % len(sel5.xpath('//*[@id="awardInfo_data"]/dd'))
-	for i in sel5.xpath('//*[@id="awardInfo_data"]/dd'):
-		print i.xpath('./h3/b/text()').extract()[0].strip()
-	# if len(sel5.xpath('//*[@id="awardInfo"]/dl/dt')) == 1:
-	# 	for i in sel5.xpath('//*[@id="awardInfo"]/dl/dd'):
-	# 		print i.xpath('./span/text()').extract()[0].strip()
-	# 		print i.xpath('./a/text()').extract()[0].strip()
+	print u'奖项种类:%s' % len(sel5.xpath('//*[@id="awardInfo_data"]/dd'))
 
-for j in movie_url_list:
-	movie_info(j)
-	print '------' * 15
+	for i in sel5.xpath('//*[@id="awardInfo_data"]/dd'):
+		format_dict = {
+						'date': '',
+						'award_body': '',
+						'award_category': '',
+						'Won': [],
+						'Nominated': [],
+						'2nd place': [],
+						'3rd place': [],
+					}
+
+		both_name = i.xpath('./h3/b/text()').extract()[0].strip()
+		format_dict['award_body'] = both_name
+		format_dict['award_category'] = both_name
+		print u'奖项名称:%s' % format_dict['award_category']
+
+		if i.xpath('.//*[@class="mr15"]/a/text()').extract():
+			format_dict['date'] = i.xpath('.//*[@class="mr15"]/a/text()').extract()[0].strip()
+		else:
+			format_dict['date'] = ''
+
+		print u'哪一届:%s' % format_dict['date']
+		
+
 	
+
+# for j in movie_url_list:
+for j in single_test:
+	movie_info(j)
+	print '------' * 10
+	# break
 
 
 
